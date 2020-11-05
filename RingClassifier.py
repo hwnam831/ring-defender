0a0918031de8d10a7f5ac2ab32bedd0441842de1
Hyoungwook Nam <hwnam831@gmail.com>
2020-11-05 13:48:25 -0600

    generator comparison complete

--- RingClassifier.py
+++ RingClassifier.py
@@ -1,4 +1,7 @@
 import RingDataset
+from Models import CNNModel, GaussianGenerator, GaussianSinusoid, RNNGenerator, MLP
+import os
+import argparse
 import numpy as np
 import pickle
 import torch
@@ -7,203 +10,103 @@ from torch.utils.data import random_split
 import torch.nn as nn
 
 
-def ConvBlockRelu(c_in, c_out, ksize, dilation=1):
-    pad = ((ksize-1)//2)*dilation
-    return nn.Sequential(
-            nn.Conv1d(c_in, c_out, ksize, 1, pad, dilation=dilation),
-            #nn.BatchNorm1d(c_out),
-            nn.ReLU())
+ 
 
-class ResBlock(nn.Module):
-    def __init__(self, c_in, c_mid, dilation=1):
-        super(ResBlock, self).__init__()
-        self.block = nn.Sequential(
-                ConvBlockRelu(c_in, c_mid, 1),
-                ConvBlockRelu(c_mid, c_mid, 5, dilation=dilation),
-                ConvBlockRelu(c_mid, c_in, 1))
-
-    def forward(self, x):
-        out = self.block(x)
-        out += x
-        return out
-
-class CNNModel(nn.Module):
-    def __init__(self, threshold, drop=0.1):
-        super().__init__()
-
-        self.CNN = nn.Sequential(
-            nn.Conv1d(1, 32, 11, 1, 5),
-            nn.Dropout(drop),
-            nn.ReLU(),
-            ResBlock(32, 16),
-            nn.Conv1d(32, 64, 5, 2, 2),
-            nn.Dropout(drop),
-            nn.ReLU(),
-            #nn.MaxPool1d(2),
-            ResBlock(64, 32),
-            nn.Conv1d(64, 128, 5, 2, 2),
-            nn.Dropout(drop),
-            nn.ReLU(),
-            #nn.MaxPool1d(2),
-        )
-        self.FC = nn.Sequential(
-            nn.Linear(128*((threshold+3)//4), 512),
-            nn.Dropout(drop),
-            nn.ReLU(),
-            nn.Linear(512,512),
-            nn.ReLU(),
-            nn.Dropout(drop),
-            nn.Linear(512,2)
-        )
-    def forward(self, x):
-        out = self.CNN(x.view(x.size(0),1,x.size(1)))
-        out = self.FC(out.view(out.size(0),-1))
-        return out
-
-class CNNGenerator(nn.Module):
-    def __init__(self, threshold, window=32, drop=0.2):
-        super().__init__()
-        self.encoder = nn.Sequential(
-            nn.Conv1d(window, 64, 1, bias=True),
-            nn.ReLU(),
-            nn.Conv1d(64, 128, 1, bias=True),
-            nn.ReLU(),
-            nn.Dropout(drop)
-        )
-        
-        self.resblock = nn.Sequential(
-                nn.Conv1d(128, 64, 1, bias=True),
-                nn.ReLU(),
-                #nn.ConstantPad1d((2,0),0.0),
-                nn.Conv1d(64, 128, 1, bias=True),
-                nn.ReLU(),
-                nn.Dropout(drop)
-            )
-
-        self.decoder = nn.Sequential(
-            nn.Conv1d(128, 1, 1, bias=True),
-            nn.ReLU(),
-        )
-
-    def forward(self, x):
-        out = self.encoder(x)
-        out = out + self.resblock(out)
-        out = self.decoder(out)
-        return out.view(out.size(0),-1)
-
-class GaussianGenerator(nn.Module):
-    def __init__(self, threshold, scale=1, window=32, drop=0.2):
-        super().__init__()
-        self.bias = nn.Parameter(torch.randn(threshold,dtype=torch.float))
-        self.scale = nn.Parameter(torch.ones(1, dtype=torch.float)*scale)
-    def forward(self,x):
-        #assuming N,C,S
-        perturb = torch.randn([x.size(0), threshold], device=x.device) #[N, S]
-        perturb = perturb*self.scale + self.bias
-        return perturb
+window=32 #this is fixed
 
 
-class GaussianSinusoid(nn.Module):
-    def __init__(self, threshold, scale=1, window=32, drop=0.2):
-        super().__init__()
-        self.bias = nn.Parameter(torch.randn(threshold,dtype=torch.float))
-        self.amp = nn.Parameter(torch.ones(1, dtype=torch.float)*scale)
-        self.freq = nn.Parameter(torch.ones(1,dtype=torch.float)/(threshold/8))
-        self.scale = nn.Parameter(torch.ones(1, dtype=torch.float)*scale)
-        self.threshold=threshold
-    def forward(self,x):
-        #assuming N,C,S
-        noise = self.scale*torch.randn([x.size(0), self.threshold], device=x.device) #[N, S]
-        omega = 2*np.pi*self.freq
-        t = torch.arange(self.threshold, device=x.device, dtype=torch.float)
-        theta = torch.randn(x.size(0),device=x.device)*(2*np.pi)
-        sinu = self.amp*torch.sin((omega*t)[None,:] + theta[:,None])
-        perturb = sinu[None,:] + noise + self.bias
-        return perturb
+def get_args():
+    """Get all the args"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+            "--net",
+            type=str,
+            choices=['cnn', 'rnn', 'ff'],
+            default='cnn',
+            help='Classifier choices')
+    parser.add_argument(
+            "--gen",
+            type=str,
+            choices=['gau', 'sin', 'adv'],
+            default='adv',
+            help='Generator choices')
+    parser.add_argument(
+            "--threshold",
+            type=int,
+            default='42',
+            help='number of samples threshold')
+    parser.add_argument(
+            "--epochs",
+            type=int,
+            default='100',
+            help='number of epochs')
+    parser.add_argument(
+            "--train_file",
+            type=str,
+            default='core4ToSlice3.pkl',
+            help='traininig dataset pkl file to load')
+    parser.add_argument(
+            "--test_file",
+            type=str,
+            default='core4ToSlice3_test.pkl',
+            help='test dataset pkl file to load')
+    parser.add_argument(
+            "--batch_size",
+            type=int,
+            default='128',
+            help='batch size')
+    parser.add_argument(
+            "--dim",
+            type=int,
+            default='128',
+            help='internal channel dimension')
+    parser.add_argument(
+            "--lr",
+            type=float,
+            default=2e-5,
+            help='Default learning rate')
+    parser.add_argument(
+            "--amp",
+            type=float,
+            default='10',
+            help='noise amp scale')
 
-class RNNGenerator(nn.Module):
-    def __init__(self, threshold, window=32, drop=0.2):
-        super().__init__()
-        self.encoder = nn.Sequential(
-            nn.Linear(window, 128),
-            nn.Dropout(drop)
-        )
-        
-        self.resblock = nn.GRU(128,128, num_layers=2, batch_first=True, dropout=drop)
+    return parser.parse_args()
 
-        self.decoder = nn.Sequential(
-            nn.Linear(128, 1)
-        )
-        self.scale = nn.Parameter(torch.ones(1))
-        self.noise = GaussianSinusoid(threshold)
-
-    def forward(self, x):
-        out = self.encoder(x.permute(0,2,1)) #N,C,S -> N,S,C
-        res, _ = self.resblock(out)
-        out = out + res #N,S,C
-        out = self.decoder(out)
-        gaussian = self.scale*torch.randn_like(out)
-        out = torch.relu(out + gaussian)
-        return out.view(out.size(0),-1)    
-
-
-def shifter(arr, window=32):
+def shifter(arr, window=window):
     dup = arr[:,None,:].expand(arr.size(0), arr.size(1)+1, arr.size(1))
     dup2 = dup.reshape(arr.size(0), arr.size(1), arr.size(1)+1)
     shifted = dup2[:,:window,:-window]
     return shifted
 
 if __name__ == '__main__':
-    threshold = 42
-    epochs = 50
-    dim=128
-    dataset = RingDataset.RingDataset('core4ToSlice3.pkl', threshold=threshold)
-    testset =  RingDataset.RingDataset('core4ToSlice3_test.pkl', threshold=threshold)
+    args = get_args()
+    dataset = RingDataset.RingDataset(args.train_file, threshold=args.threshold)
+    testset =  RingDataset.RingDataset(args.test_file, threshold=args.threshold)
     #testlen = dataset.__len__()//4
     #trainlen = dataset.__len__() - testlen
     #testset, trainset = random_split(dataset, [testlen, trainlen], generator=torch.Generator().manual_seed(17))
     #testset, trainset = random_split(dataset, [testlen, trainlen])
     trainset=dataset
-    trainloader = DataLoader(trainset, batch_size=64, num_workers=2)
-    testloader = DataLoader(testset, batch_size=64)
-    MLP = nn.Sequential(
-        nn.Linear(threshold,dim//2),
-        nn.ReLU(),
-        nn.Dropout(0.25),
-        nn.Linear(dim//2,dim),
-        nn.ReLU(),
-        nn.Dropout(0.25),
-        nn.Linear(dim,dim),
-        nn.ReLU(),
-        nn.Dropout(0.25),
-        nn.Linear(dim,2)
-    ).cuda()
-    MLPgen = nn.Sequential(
-        nn.Linear(threshold,dim//2),
-        nn.ReLU(),
-        nn.Dropout(0.25),
-        nn.Linear(dim//2,dim),
-        nn.ReLU(),
-        nn.Linear(dim,dim),
-        nn.ReLU(),
-        nn.Dropout(0.25),
-        nn.Linear(dim,threshold),
-        nn.ReLU()
-    ).cuda()
-    cnn = CNNModel(threshold).cuda()
-    #gen = CNNGenerator(threshold).cuda()
-    gen=RNNGenerator(threshold).cuda()
-    GG = nn.Sequential(
-        GaussianGenerator(threshold, scale=20),
-        nn.ReLU(),
-    ).cuda()
+    trainloader = DataLoader(trainset, batch_size=args.batch_size, num_workers=4)
+    testloader = DataLoader(testset, batch_size=args.batch_size, num_workers=4)
     
-    GS = nn.Sequential(
-        GaussianSinusoid(threshold, scale=20),
+    if args.gen == 'gau':
+        gen = nn.Sequential(
+        GaussianGenerator(args.threshold, scale=args.amp),
         nn.ReLU(),
-    ).cuda()
-    #gen = GS
-    #gen=MLPgen
+        ).cuda()
+    elif args.gen == 'sin':
+        gen = nn.Sequential(
+        GaussianSinusoid(args.threshold, scale=args.amp),
+        nn.ReLU(),
+        ).cuda()
+    elif args.gen == 'adv':
+        gen=RNNGenerator(args.threshold).cuda()
+    else:
+        print(args.gen + ' not supported\n')
+        exit(-1)
+    
     '''
     for x,y in trainloader:
         xp = gen(x.cuda())
@@ -211,14 +114,16 @@ if __name__ == '__main__':
         print(xp.shape)
         break
     '''
-    classifier = cnn
-    optim_c = torch.optim.Adam(classifier.parameters(), lr=2e-5)
-    optim_g = torch.optim.Adam(gen.parameters(), lr=4e-5)
+    if args.net == 'ff':
+        classifier = MLP(args.threshold, args.dim).cuda()
+    else:
+        classifier = CNNModel(args.threshold).cuda()
+    optim_c = torch.optim.Adam(classifier.parameters(), lr=args.lr)
+    optim_g = torch.optim.Adam(gen.parameters(), lr=args.lr*2)
 
     criterion = nn.CrossEntropyLoss()
-    C = 40.0
-    repeat=2
-    warmup = 5
+    C = args.amp * 4
+    warmup = 10
     scale = 0.001
 
     for e in range(warmup):
@@ -233,7 +138,7 @@ if __name__ == '__main__':
             loss_c.backward()
             optim_c.step()
 
-    for e in range(epochs):
+    for e in range(args.epochs):
         gen.train()
         classifier.train()
         for x,y in trainloader:
@@ -266,7 +171,8 @@ if __name__ == '__main__':
             optim_g.step()
 
         mloss = 0.0
-        macc = 0.0
+        totcorrect = 0
+        totcount = 0
         mnorm = 0.0
         #evaluate classifier
         with torch.no_grad():
@@ -283,7 +189,10 @@ if __name__ == '__main__':
                 pred = output.argmax(axis=-1)
                 mnorm += norm.item()/len(testloader)
                 mloss += loss_c.item()/len(testloader)
-                macc += ((pred==ydata).sum().float()/pred.nelement()).item()/len(testloader)
+                #macc += ((pred==ydata).sum().float()/pred.nelement()).item()/len(testloader)
+                totcorrect += (pred==ydata).sum().item()
+                totcount += y.size(0)
+            macc = float(totcorrect)/totcount
             print("epoch {} \t acc {:.6f}\t loss {:.6f}\t Avg perturb {:.6f}\n".format(e+1, macc, mloss, mnorm))
 
 
